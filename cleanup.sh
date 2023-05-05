92664853224151f558d0581bc5b42620da2bf2fb
Dean Bodenham <X>
2023-05-05 17:10:20 +0100

    updating cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,11 @@
-# move into folder
-cd egpkg_package
-
 # remove all files/folders from build
-rm -rf dist/
-rm -rf egpkg.egg-info/
-rm -rf egpkg/src/__pycache__/
-rm -rf egpkg/cppsrc/__pycache__/
-
-# move back out of folder
-cd ..
+rm -rf egpkg_package/dist
+rm -rf egpkg_package/egpkg.egg-info
+rm -rf egpkg_package/egpkg/src/__pycache__
+rm -rf egpkg_package/egpkg/cppsrc/__pycache__
+rm -rf egpkg_package/.pytest_cache
+rm -rf egpkg_package/build
+rm -rf egpkg_package/egpkg/__pycache__
+rm -rf egpkg_package/egpkg/cppsrc/*.so
+rm -rf egpkg_package/egpkg/cppsrc/cppmethods.cpp
+rm -rf egpkg_package/tests/__pycache__
